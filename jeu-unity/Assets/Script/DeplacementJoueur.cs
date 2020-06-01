f6840705353fb7fed6db6fffe5b06c8c0846a007
ArphantBastien <arphant.bastienn@gmail.com>
2020-06-02 00:23:13 +0200

    2eme depot

--- jeu-unity/Assets/Script/DeplacementJoueur.cs
+++ jeu-unity/Assets/Script/DeplacementJoueur.cs
@@ -8,33 +8,37 @@ public class DeplacementJoueur : MonoBehaviour
     public bool isJumping;
     public bool isGrounded;
 
-    public Transform groundCheckLeft;
-    public Transform groundCheckRight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
 
     public Rigidbody2D rb;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
 
     private Vector3 velocity = Vector3.zero;
+    private float horizontalMovement;
   
-    void FixedUpdate()
+    void Update()
     {
-        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
 
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        
-        if(Input.GetButtonDown("Jump") && isGrounded)
+         horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
         }
 
-        MovePlayer(horizontalMovement);
         Flip(rb.velocity.x);
 
         float characterVelocity = Mathf.Abs(rb.velocity.x);
         animator.SetFloat("Speed", characterVelocity);
     }
-
+    void FixedUpdate()
+    { 
+        MovePlayer(horizontalMovement);
+       }   
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
@@ -57,5 +61,11 @@ public class DeplacementJoueur : MonoBehaviour
         {
             spriteRenderer.flipX = true;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
